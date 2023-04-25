761764eb35fac478a8303578737c9fe94bcf0d4c
MishaBelove96 <mishabelove72@gmail.com>
2023-04-26 00:34:04 +0400

    beginning and creating components card and drawer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import Drawer from "./components/drawer";
+import AppContext from "./context";
+
+import Home from "./pages/Home";
+import Favorites from "./pages/Favorites";
+import Orders from "./pages/Orders";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="wrapper">
+      <header>
+        <div className="headerLeft">
+          <svg />
+          <div className="headerInfo">
+            <h3>Sneakers World</h3>
+            <p>The best sneakers in the world!</p>
+          </div>
+        </div>
+        <ul className="headerRight">
+          <li>
+            <svg />
+            <span>120 $</span>
+          </li>
+          <li>
+            <svg />
+          </li>
+        </ul>
       </header>
+      <div className="component"></div>
     </div>
   );
 }
